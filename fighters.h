9d9d676ea19532fd9364a63a9f6a0f3e644c913c
nikol4o99 <nikola991210@gmail.com>
2020-05-14 01:32:58 +0300

    Add files via upload
    Едно време, в дивия северозапад, хората са се пребивали най-редовно, но пък за
    сметка на това, след всеки бой, тези които са още в съзнание, продължавали да пият и
    така, ако започнат нов кютек, все едно са били напълно здрави. Това най-често били
    победителите... а победените - лежали в безсъзнание някъде по градинките. Направете
    &quot;турнир&quot; от биячи, които са записани в книгата на кръчмата. Зареждайте имената им в
    контейнер, след което ги сбивайте по двойки. Приемете, че всеки почерпан има по
    100% здраве и никой не може да му нанесе повече от 20% щети. Биячите се бият
    последователно - първо удря единият, после другият - не е честно, ама така е в живия
    живот. Естествено - бият се докато единият не победи. Генерирайте двубоите чрез
    rand(). След всеки всеки кръг двубой, направете нови двойки от победителите и така
    по метода на елиминирането в края обявете победител.

---
+++ fighters.h
@@ -0,0 +1,61 @@
+#ifndef FIGHTERS_H
+#define FIGHTERS_H
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NAME_SIZE 20
+
+
+static size_t fighterArraySize;
+
+typedef struct Fighters
+{
+    char name[NAME_SIZE];
+} Fighter;
+typedef struct plinkedList
+{
+    Fighter currentFighter;
+    struct plinkedList * previous;
+    struct plinkedList * next;
+} FighterArray;
+
+FighterArray * newFighter(FighterArray * node,Fighter fighter)
+{
+    FighterArray * newNode = NULL;
+    newNode = (FighterArray *) malloc(sizeof(FighterArray));
+    if(newNode!=NULL)
+    {
+    node -> next = newNode;
+    newNode -> previous = node;
+    newNode -> currentFighter = fighter;
+    newNode -> next = NULL;
+    fighterArraySize++;
+    }
+    else newNode = newFighter(node,fighter);
+    return newNode;
+}
+FighterArray * fighterLoad(FighterArray * fightersLoaded)
+{
+    FILE * fp;
+    char path[] = "Fighters.txt";
+    fp = fopen(path,"r+");
+    FighterArray * first = fightersLoaded;
+    size_t fileSize;
+    fscanf(fp,"%d\n",&fileSize);
+//printf("%d",fileSize);
+    for(int i = 0 ; i < fileSize ; i++)
+    {
+        char name[NAME_SIZE];
+        Fighter new_Fighter;
+        fscanf(fp," %s/n",name);
+        //printf("%s\n",name);
+        strcpy(new_Fighter.name,name);
+        fightersLoaded = newFighter(fightersLoaded,new_Fighter);
+    }
+    fclose(fp);
+    return fightersLoaded;
+}
+
+#endif // FIGHTERS_H
---
+++ main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "fighters.h"
+#define START_HEALTH 100
+
+FighterArray * start;
+
+int fight(Fighter fighter1, Fighter fighter2)
+{
+    srand (time(NULL));
+    float fH1 = START_HEALTH;
+    float fH2 = START_HEALTH;
+    while(fH1>0&&fH2>0){
+        float damage = rand()%20;
+        printf("%s hits %s with %f damage\n",fighter1.name,fighter2.name,damage);
+        fH2 -= damage;
+        if(fH2>0){
+        damage = rand()%20;
+        printf("%s hits %s with %f damage\n",fighter2.name,fighter1.name,damage);
+        fH1 -= damage;
+        }
+    }
+    if(fH1 <=0) return 2;
+    else return 1;
+}
+FighterArray * pairing(FighterArray * begNode,FighterArray * endNode,int fightSize)
+{
+    int fightNum = fightSize,k = 0;
+    FighterArray * newArr;
+    if(fightNum%2 != 0) {
+        fightNum--;
+        newArr = newFighter(newArr,begNode->currentFighter);
+        start = newArr;
+        begNode = begNode->next;
+        free(begNode->previous);
+        k++;
+    }
+    fightNum/=2;
+    for(int i=0;i<fightNum;i++){
+        int fightRes = fight(begNode->currentFighter,endNode->currentFighter);
+        if(fightRes == 1) {
+                printf("Winner is: %s\n",begNode->currentFighter.name);
+                newArr = newFighter(newArr,begNode->currentFighter);
+        }
+        else {
+                printf("Winner is: %s\n",endNode->currentFighter.name);
+                newArr = newFighter(newArr,endNode->currentFighter);
+        }
+        if(i==0&&k==0)start = newArr;
+        endNode = endNode->previous;
+        free(endNode->next);
+        begNode = begNode->next;
+        free(begNode->previous);
+    }
+    fightSize+= fightNum;
+    return newArr;
+}
+
+int main()
+{
+
+    FighterArray * fighterArray = NULL;
+    fighterArray = (FighterArray *) malloc(sizeof(FighterArray));
+    if(fighterArray != NULL) fighterArray  = fighterLoad(fighterArray);
+    start = fighterArray;
+    for(int i=0;i<fighterArraySize-1;i++){
+        start = start->previous;
+    }
+    int fsize = (int)fighterArraySize;
+    //printf("%s\n\n",start->currentFighter.name);
+    while(fsize>=2){
+        fighterArray = pairing(start,fighterArray,fsize);
+        if(fsize%2==0) fsize/=2;
+        else{
+            fsize+=1;
+            fsize/=2;
+        }
+        system("pause");
+    }
+    printf("CHAMION IS: %s\n",fighterArray->currentFighter.name);
+
+    return 0;
+}
